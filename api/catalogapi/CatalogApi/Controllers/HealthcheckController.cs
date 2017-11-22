046235b7b8a4a63e262ada561fa9710c842ecb0d
Sandeep Chugh <sandeep.chugh@live.com>
2017-11-21 18:47:03 -0600

    Added healthcheck controller

---
+++ api/catalogapi/CatalogApi/Controllers/HealthcheckController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthcheckController : Controller
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            string hostname = Dns.GetHostName();
+            var ipAddress = Dns.GetHostEntry(hostname).AddressList.FirstOrDefault();
+            var ip = string.Empty;
+            if (ipAddress != null)
+                ip = ipAddress.ToString();
+
+            return new List<string>{ip,hostname};
+        }
+    }
+}
