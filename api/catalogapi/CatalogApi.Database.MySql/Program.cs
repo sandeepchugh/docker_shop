f8110bc45812073705cf54b8fc9568ca3f61f2d3
Sandeep Chugh <sandeep.chugh@live.com>
2017-11-23 11:35:35 -0600

    Added DBup project for Mysql database and docker compose file with mysql as a container

---
+++ api/catalogapi/CatalogApi.Database.MySql/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace CatalogApi.Database.MySql
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "server=localhost;database=catalogdb;uid=root;pwd=catalogrootpassword;Allow User Variables=True"; //TODO: Read from appsettings
+
+            EnsureDatabase.For.MySqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .MySqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
